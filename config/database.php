1252c98c8035129d157e83edeaeb465e568bb52a
小丑路人 <2278757482@qq.com>
2023-04-18 19:42:47 +0800

    定义User库

--- config/database.php
+++ config/database.php
@@ -62,6 +62,26 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        // User库
+        'user_mysql' => [
+            'driver' => 'mysql',
+            'url' => env('USER_DATABASE_URL', env('DATABASE_URL')),
+            'host' => env('DB_USER_HOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('DB_USER_PORT', env('DB_PORT', '3306')),
+            'database' => env('DB_USER_DATABASE', env('DB_DATABASE', 'forge')),
+            'username' => env('DB_USER_USERNAME', env('DB_USERNAME', 'forge')),
+            'password' => env('DB_USER_PASSWORD', env('DB_PASSWORD', '')),
+            'unix_socket' => env('DB_USER_SOCKET', env('DB_SOCKET', '')),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => env('DB_USER_PREFIX', env('DB_PREFIX', '')),
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
