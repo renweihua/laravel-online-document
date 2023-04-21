bff52e6e18f8cef1aadf1b9c4ab555a426b29f1a
小丑路人 <2278757482@qq.com>
2023-04-21 14:43:45 +0800

    定义文档表结构与模型

---
+++ app/Models/Docs/Doc.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Docs;
+
+use App\Models\Model;
+use App\Models\UserInfo;
+
+class Doc extends Model
+{
+    protected $appends = ['time_formatting'];
+
+    public function userInfo()
+    {
+        return $this->belongsTo(UserInfo::class, 'user_id', 'user_id');
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'project_id', 'project_id');
+    }
+}
---
+++ app/Modules/Docs/Database/Migrations/2023_04_21_140534_create_docs_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateDocsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $table = 'docs';
+        if (Schema::hasTable($table)) return;
+        Schema::create($table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id('');
+            $table->bigInteger('user_id')->unsigned()->default(0)->comment('会员Id');
+            $table->bigInteger('project_id')->unsigned()->default(0)->comment('项目Id');
+            $table->bigInteger('group_id')->unsigned()->default(0)->comment('分组Id');
+            $table->string('doc_name', 100)->default('')->comment('文档名称');
+            $table->longtext('content_html')->nullable()->comment('内容');
+            $table->longtext('content_markdown')->nullable()->comment('md内容');
+            $table->integer('sort')->unsigned()->default(0)->comment('排序：升序');
+            $table->integer('created_time')->unsigned()->default(0)->comment('创建时间');
+            $table->integer('updated_time')->unsigned()->default(0)->comment('更新时间');
+            $table->boolean('is_delete')->unsigned()->default(0)->comment('是否删除');
+            $table->integer('view_count')->unsigned()->default(0)->comment('浏览量');
+            $table->index('doc_name');
+            $table->index('user_id');
+            $table->index(['project_id', 'group_id']);
+            $table->index('sort');
+            $table->index('is_delete');
+        });
+        $table = get_db_prefix() . $table;
+        // 设置表注释
+        Db::statement("ALTER TABLE `{$table}` comment '文档表'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('docs');
+    }
+}
