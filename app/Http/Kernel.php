936772e9b966db2d402d3e8b94ed0f6dd948f4d1
小丑路人 <2278757482@qq.com>
2023-04-22 16:49:59 +0800

    定义`ConvertEmptyStringsToNull`替换`\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class`

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\ConvertEmptyStringsToNull;
 use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -21,7 +22,12 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+
+        // 空字符串自动转换为null的中间件
+        // \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+
+        // 自定义的：空字符串自动转换为null的中间件
+        ConvertEmptyStringsToNull::class,
     ];
 
     /**
