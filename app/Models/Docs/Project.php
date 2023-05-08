41e1755bb9d2c3b39d751f1362254062d84122d7
小丑路人 <2278757482@qq.com>
2023-05-08 10:56:31 +0800

    checkRolePower 验证项目的访问权限：role_power 0.访问权限；1.编辑权限

--- app/Models/Docs/Project.php
+++ app/Models/Docs/Project.php
@@ -38,17 +38,35 @@ class Project extends Model
         return self::find($id);
     }
 
-    // 验证访问权限
-    public static function checkPower($project)
+    // 验证项目的访问权限：role_power 0.访问权限；1.编辑权限
+    public static function checkRolePower($project, $role_power = ProjectMember::ROLE_POWER_READ)
     {
         $login_user_id = getLoginUserId();
-        // 非创建人
-        if ($project->user_id != $login_user_id){
+
+        // 创建人
+        if ($project->user_id == $login_user_id){
+            return true;
+        }
+        if ($role_power == ProjectMember::ROLE_POWER_READ){ // 访问权限
+            // 公开项目
+            if ($project->is_public == 1){
+                return true;
+            }
+            // 项目成员皆有访问权限
+            if (ProjectMember::where('project_id', $project->project_id)->where('user_id', $login_user_id)->first()){
+                return true;
+            }
+        }else{
             // 是否为项目成员
-            if (!ProjectMember::where('project_id', $project->project_id)->where('user_id', $login_user_id)->first()){
+            $member = ProjectMember::where('project_id', $project->project_id)->where('user_id', $login_user_id)->first();
+            if (!$member){
                 return false;
             }
+            // 项目管理员有编辑权限、项目成员有编辑权限
+            if ($member->is_leader == 1 || $member->role_power == ProjectMember::ROLE_POWER_WRITE){
+                return true;
+            }
         }
-        return true;
+        return false;
     }
 }
